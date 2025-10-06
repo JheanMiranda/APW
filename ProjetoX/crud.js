423e3ffc25106119e9374cbcb3d40147b6220cf9
JheanMiranda <jhean.miranda@etec.sp.gov.br>
2025-10-06 18:17:50 -0300

    Add files via upload

---
+++ ProjetoX/crud.js
@@ -0,0 +1,18 @@
+import connection from './bd;js'
+
+ export class Crud {
+
+  //M�todo - sele��o
+  async inserir() {
+	let conn = connection()
+
+	let [row] = conn.query('select * from paciente')
+	console.log('Pacientes cadastrados!' + rows)
+
+	} catch (error) {
+	    console.error('erro de sele��o')
+	    conn.end()
+	}
+	
+  }
+
